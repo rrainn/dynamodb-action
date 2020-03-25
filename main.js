7baf2ad9c83cbc96e708a4b243456f46f53163a2
Charlie Fish <fishcharlie@me.com>
2020-03-24 20:20:36 -0600

    Adding support for more options

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
 
+const settings = ["port", "dbPath", "sharedDb", "cors", "delayTransientStatuses", "optimizeDbBeforeStartup"].reduce((obj, key) => {
+	obj[key] = core.getInput(key);
+	return obj;
+}, {});
+
 (async function (){
-	await exec.exec("sudo docker run --name dynamodb -d -p 8000:8000 amazon/dynamodb-local");
+	const extraArguments = [
+		settings.dbPath ? `-dbPath ${settings.dbPath}` : "-inMemory",
+		settings.sharedDb ? "-sharedDb" : null,
+		settings.cors ? `-cors ${settings.cors}` : null
+		settings.delayTransientStatuses ? `-delayTransientStatuses` : null,
+		settings.optimizeDbBeforeStartup ? `-optimizeDbBeforeStartup` : null
+	].filter((a) => Boolean(a)).join(" ");
+	await exec.exec(`sudo docker run --name dynamodb -d -p ${settings.port}:${settings.port} amazon/dynamodb-local -jar DynamoDBLocal.jar -port ${settings.port}${extraArguments ? ` ${extraArguments}` : ""}`);
 })();
